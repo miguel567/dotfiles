18c7176c40184ad546617e1d2ab2c3758351bb5d
LAPTOP-BMG44977\migue <miguel567@gmail.com>
2020-03-14 16:33:54 +0100

    added aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -3,6 +3,8 @@ case `uname` in
     alias ls='/usr/local/bin/gls --color -h --group-directories-first'
   ;;
   Linux)
+    alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
+    alias dapps='. "/home/migue//.nix-profile/etc/profile.d/nix.sh" && echo ". "/home/migue//.nix-profile/etc/profile.d/nix.sh""'
     alias ls='/bin/ls --color=tty --group-directories-first'
   ;;
 esac
